22431be8f536062ca09855ea33063a4435c0e4c6
Estebans441 <estebans441@gmail.com>
2023-02-08 12:16:34 -0500

    initial commit

---
+++ SVM_dataset_1.py
@@ -0,0 +1,195 @@
+# importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# generating the dataset
+from sklearn.datasets import make_circles
+
+X, y = make_circles(n_samples=1000, shuffle=True, noise=0.25, factor=0)
+
+# Splitting the dataset
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+print("----------")
+print("Linear")
+print("----------")
+# Training the SVM model with linear kernel
+from sklearn.svm import SVC
+
+classifier = SVC(kernel='linear', random_state=0)
+classifier.fit(X_train, y_train)
+
+# Predicting the test set results
+y_pred = classifier.predict(X_test)
+print("Predicts")
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1)[:10])
+
+# Making the confusion matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion matrix")
+print(cm)
+print(accuracy_score(y_test, y_pred))
+
+# Visualising the training set results
+from matplotlib.colors import ListedColormap
+
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.1),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.1))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('lightblue', 'cadetblue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('orange', 'blue'))(i), label=j)
+plt.title('SVM Linear (Training set)')
+plt.xlabel('X1')
+plt.ylabel('X2')
+plt.legend()
+plt.show()
+
+# Visualising the test set results
+from matplotlib.colors import ListedColormap
+
+X_set, y_set = sc.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.1),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.1))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('lightblue', 'cadetblue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('orange', 'blue'))(i), label=j)
+plt.title('SVM Linear (Test set)')
+plt.xlabel('X1')
+plt.ylabel('X2')
+plt.legend()
+plt.show()
+
+print("----------")
+print("RBF")
+print("----------")
+# Training the SVM model with linear kernel
+from sklearn.svm import SVC
+
+classifier = SVC(kernel='rbf', random_state=0)
+classifier.fit(X_train, y_train)
+
+# Predicting the test set results
+y_pred = classifier.predict(X_test)
+print("Predicts")
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1)[:10])
+
+# Making the confusion matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion matrix")
+print(cm)
+print(accuracy_score(y_test, y_pred))
+
+# Visualising the training set results
+from matplotlib.colors import ListedColormap
+
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.1),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.1))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('lightblue', 'cadetblue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('orange', 'blue'))(i), label=j)
+plt.title('SVM rbf (Training set)')
+plt.xlabel('X1')
+plt.ylabel('X2')
+plt.legend()
+plt.show()
+
+# Visualising the test set results
+from matplotlib.colors import ListedColormap
+
+X_set, y_set = sc.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.1),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.1))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('lightblue', 'cadetblue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('orange', 'blue'))(i), label=j)
+plt.title('SVM rbf (Test set)')
+plt.xlabel('X1')
+plt.ylabel('X2')
+plt.legend()
+plt.show()
+
+print("----------")
+print("Poly")
+print("----------")
+# Training the SVM model with linear kernel
+from sklearn.svm import SVC
+
+classifier = SVC(kernel='poly', random_state=0)
+classifier.fit(X_train, y_train)
+
+# Predicting the test set results
+y_pred = classifier.predict(X_test)
+print("Predicts")
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1)[:10])
+
+# Making the confusion matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion matrix")
+print(cm)
+print(accuracy_score(y_test, y_pred))
+
+# Visualising the training set results
+from matplotlib.colors import ListedColormap
+
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.1),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.1))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('lightblue', 'cadetblue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('orange', 'blue'))(i), label=j)
+plt.title('SVM Poly (Training set)')
+plt.xlabel('X1')
+plt.ylabel('X2')
+plt.legend()
+plt.show()
+
+# Visualising the test set results
+from matplotlib.colors import ListedColormap
+
+X_set, y_set = sc.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.1),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.1))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('lightblue', 'cadetblue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('orange', 'blue'))(i), label=j)
+plt.title('SVM Poly (Test set)')
+plt.xlabel('X1')
+plt.ylabel('X2')
+plt.legend()
+plt.show()
